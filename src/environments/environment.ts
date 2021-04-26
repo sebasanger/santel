035544f796026e3390de94ec5e00e51d0eeb1871
Seba pc <seba_sanger@hotmail.com>
2021-04-26 19:02:36 -0300

    add production flag to firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:8080/',
+  base_url: 'https://sprintel.herokuapp.com/',
   client_url: 'http://localhost:4200/',
 };
 
